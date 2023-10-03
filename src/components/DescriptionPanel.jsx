ee0c44fac27e8737543376f06d8937a6d7979c44
Degorce-dumasmarilyn <113439653+Degorce-dumasmarilyn@users.noreply.github.com>
2023-10-03 12:24:07 +0200

    description panel

--- src/components/DescriptionPanel.jsx
+++ src/components/DescriptionPanel.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import "./DescriptionPanel.scss";
 
 export function DescriptionPanel(props) {
-  const [isContentVisible, setIsContentVisible] = useState(true);
+  const [isContentVisible, setIsContentVisible] = useState(false);
 
   const showContent = () => {
     setIsContentVisible(!isContentVisible);
